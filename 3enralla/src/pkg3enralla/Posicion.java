53f4cab5c8ee17e4b3937c2b52abcb3683ecb5df
Mofitex <Mofitex@DESKTOP-NA01H5D>
2018-04-15 20:01:06 +0200

    Añadir constructor y getters/setters de Posicion

--- 3enralla/src/pkg3enralla/Posicion.java
+++ 3enralla/src/pkg3enralla/Posicion.java
@@ -12,5 +12,26 @@ package pkg3enralla;
 public class Posicion {
     private int x;
     private int y;
+
+    public Posicion(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
     
 }
